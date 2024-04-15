063a25414d8639acc9ac9b11832238a97035b0c2
wanderlei jose bolzan <wbolzanjp@gmail.com>
2024-04-15 09:34:48 -0300

    Validation exception

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,9 +1,12 @@
+
 package gui;
 
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listners.DataChangeListener;
@@ -18,15 +21,16 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
-		
+
 	private Department entity;
 	
 	private DepartmentService service;
 	
-	private List<DataChangeListener> dataChangeListener = new ArrayList<>();
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
 	
 	@FXML
 	private TextField txtId;
@@ -35,13 +39,25 @@ public class DepartmentFormController implements Initializable {
 	private TextField txtName;
 	
 	@FXML
-	private Label labelErroName;
+	private Label labelErrorName;
 	
 	@FXML
 	private Button btSave;
 	
 	@FXML
 	private Button btCancel;
+	
+	public void setDepartment(Department entity) {
+		this.entity = entity;
+	}
+	
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
+	}
+	
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
+	}
 	
 	@FXML
 	public void onBtSaveAction(ActionEvent event) {
@@ -54,25 +70,38 @@ public class DepartmentFormController implements Initializable {
 		try {
 			entity = getFormData();
 			service.saveOrUpdate(entity);
-			notifyDataChangeListners();
+			notifyDataChangeListeners();
 			Utils.currentStage(event).close();
+		}
+		catch (ValidationException e) {
+			setErrorMessages(e.getErrors());
 		}
 		catch (DbException e) {
 			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
 		}
 	}
 	
-	private void notifyDataChangeListners() {
-		for (DataChangeListener listener : dataChangeListener) {
+	private void notifyDataChangeListeners() {
+		for (DataChangeListener listener : dataChangeListeners) {
 			listener.onDataChanged();
 		}
-		
 	}
 
 	private Department getFormData() {
 		Department obj = new Department();
-		obj.setId(Utils.tryParseToInt(txtId.getId()));
+		
+		ValidationException exception = new ValidationException("Validation error");
+		
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		
+		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
+			exception.addError("name", "Field can't be empty");
+		}
 		obj.setName(txtName.getText());
+		
+		if (exception.getErrors().size() > 0) {
+			throw exception;
+		}
 		
 		return obj;
 	}
@@ -81,22 +110,10 @@ public class DepartmentFormController implements Initializable {
 	public void onBtCancelAction(ActionEvent event) {
 		Utils.currentStage(event).close();
 	}
-
-	public void setDepartment(Department entity ) {
-		this.entity = entity;
-	}
-	
-	public void setDepartmentService(DepartmentService service) {
-		this.service = service;
-	}
-	
-	public void subscribeDataChangeListener(DataChangeListener listener) {
-		dataChangeListener.add(listener);
-	}
 	
 	@Override
-	public void initialize(URL url, ResourceBundle rs) {		
-		initializeNodes();		
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
 	}
 	
 	private void initializeNodes() {
@@ -109,7 +126,14 @@ public class DepartmentFormController implements Initializable {
 			throw new IllegalStateException("Entity was null");
 		}
 		txtId.setText(String.valueOf(entity.getId()));
-	    txtName.setText(entity.getName());
+		txtName.setText(entity.getName());
 	}
-
+	
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		
+		if (fields.contains("name")) {
+			labelErrorName.setText(errors.get("name"));
+		}
+	}
 }
